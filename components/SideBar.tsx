72fcb3115e8f6210f31c99d39d798a25d452b357
dinhdiem <miktendinh@gmail.com>
2022-07-21 00:58:07 +0700

    update sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,16 +1,54 @@
 import { Avatar, Button, IconButton, Tooltip } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ChatIcon from "@mui/icons-material/Chat";
 import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from "@mui/icons-material/Logout";
 import MoveVertIcon from "@mui/icons-material/MoreVert";
 import { signOut } from "firebase/auth";
-import { auth } from "../config/firebase";
+import { auth, db } from "../config/firebase";
+import * as EmailValidator from "email-validator";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  DialogActions,
+} from "@mui/material";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { addDoc, collection } from "firebase/firestore";
 
 type Props = {};
 
 const SideBar = (props: Props) => {
+  const [loggedInUser, _loading, _error] = useAuthState(auth);
+
+  const [isOpenNew, setIsOpenNew] = useState(false);
+
+  const [writeEmail, setWriteEmail] = useState("");
+
+  const isInvitingSelf = writeEmail === loggedInUser?.email;
+
+  const toggleNewDialog = (isOpen: boolean) => {
+    setIsOpenNew(isOpen);
+    if (!isOpen) setWriteEmail("");
+  };
+
+  const closeDialog = () => {
+    toggleNewDialog(false);
+  };
+
+  const createConversation = async () => {
+    if (!writeEmail) return;
+    if (EmailValidator.validate(writeEmail) && !isInvitingSelf) {
+      await addDoc(collection(db, "conversations"), {
+        users: [loggedInUser?.email, writeEmail],
+      });
+    }
+    closeDialog();
+  };
+
   const logout = async () => {
     try {
       await signOut(auth);
@@ -21,8 +59,8 @@ const SideBar = (props: Props) => {
   return (
     <StyleContainer>
       <StyledHeader>
-        <Tooltip title="Username" placement="right">
-          <StyledUserAvatar />
+        <Tooltip title={loggedInUser?.email as string} placement="right">
+          <StyledUserAvatar src={loggedInUser?.photoURL || ""} />
         </Tooltip>
         <div>
           <IconButton>
@@ -40,7 +78,37 @@ const SideBar = (props: Props) => {
         <SearchIcon />
         <StyledInputSearch placeholder="Tìm kiếm người dùng" />
       </StyledSearch>
-      <StyledSideBarButton>Thêm người liên hệ</StyledSideBarButton>
+      <StyledSideBarButton
+        onClick={() => {
+          toggleNewDialog(true);
+        }}
+      >
+        Thêm người liên hệ
+      </StyledSideBarButton>
+      <Dialog open={isOpenNew} onClose={closeDialog}>
+        <DialogTitle>Thêm liên hệ</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Vui lòng nhập địa chỉ người bạn muốn trò chuyện!
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Địa chỉ email"
+            type="email"
+            fullWidth
+            variant="standard"
+            onChange={(event) => setWriteEmail(event.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeDialog}>Hủy</Button>
+          <Button disabled={!writeEmail} onClick={createConversation}>
+            Tạo mới
+          </Button>
+        </DialogActions>
+      </Dialog>
     </StyleContainer>
   );
 };
